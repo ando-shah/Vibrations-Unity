57d8bdb82bbd44c61fcc0a69c617a0835c65fb78
Ando Shah <ananda13@gmail.com>
2015-06-04 17:27:17 +0530

    Added a HUD layer that come on when the trigger is pushed/screen is tapped. That action results in pausing the video and showing the HUD layer. It disappears on retap/trigger.

--- Assets/Trigger.cs
+++ Assets/Trigger.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Trigger : MonoBehaviour {
 
 	public MediaPlayerCtrl Player;
+	public GameObject HUD;
+	private Image img;
+	private Color clr;
 
 	void OnEnable()
 	{
@@ -15,18 +19,40 @@ public class Trigger : MonoBehaviour {
 		Cardboard.SDK.OnTrigger -= PlayPause;
 	}
 
+	void Start()
+	{
+		//img = HUD.GetComponentsInChildren<Image>["CanvasHUD"] ();
+		//img = HUD.GetComponent ("LowerBar") as Image;
+		//clr = img.color;
+		HUD.SetActive (false);
+
+	}
+
+
 
 	void PlayPause()
 	{
+
+
 		if (Player.GetCurrentState () == MediaPlayerCtrl.MEDIAPLAYER_STATE.PAUSED) {
 			Player.Play ();
+
+			HUD.SetActive(false)	;
+
 			Debug.Log ("\n Play");
+
+
 		} else if (Player.GetCurrentState () == MediaPlayerCtrl.MEDIAPLAYER_STATE.PLAYING) {
 			Player.Pause ();
+			HUD.SetActive(true);
+
 			Debug.Log ("\n Pause");
+
 		}
 
 	}
+
+
 
 
 
